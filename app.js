c69faf2a122502d51475c99145e8f774b02acdda
jessen <ndt94hvnh@gmail.com>
2019-04-30 16:33:13 +0700

    Lesson 1 - 6 update

---
+++ app.js
@@ -0,0 +1,52 @@
+// Lesson 5: Log to console
+
+// alert('Hello World');
+// console.log('Test');
+// console.log(123);
+// var greeting = 'Hello';
+// console.log(greeting);
+// console.table({ a: 1, b: 2 });
+// console.error('This is some error');
+// console.clear();
+// console.warn('This is a warning');
+// console.time('Hello');
+// console.log('Hello World');
+// console.log('Hello World');
+// console.log('Hello World');
+// console.log('Hello World');
+// console.log('Hello World');
+// console.timeEnd('Hello');
+
+/*
+    multiline
+    comment
+*/
+
+// Lesson 6: var, let, const
+
+// var name = 'Jone Doe';
+// console.log(name);
+// name = 'Steve Smith';
+// console.log(name);
+
+// // Init var
+// var greeting;
+// console.log(greeting);
+// greeting = 'Hello';
+// console.log(greeting);
+
+// let
+// let name = 'Jone Doe';
+// console.log(name);
+// name = 'Steve Smith';
+// console.log(name);
+
+// const
+// const name = 'John';
+// console.log(name);
+// cannot reassigin
+// name = 'Sarah';
+// Have to reassign value
+// const greeting;
+
+// Lesson 7: Datatypes
